8cc66820c5d06f21bcb6f903468bf4560aced651
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-10-13 19:17:48 +0330

    fixing the error message of not having any recipe from the beginning

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -2450,7 +2450,7 @@ class View {
         this._parentElement.insertAdjacentHTML("afterbegin", markup);
     }
     update(data) {
-        if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
+        // if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const newMarkup = this._generateMarkup();
         const newDOM = document.createRange().createContextualFragment(newMarkup);
