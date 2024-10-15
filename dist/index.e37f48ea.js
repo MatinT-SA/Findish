b59704dcfaacab66dd0778a69c1c360734e9f06d
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-10-15 21:27:13 +0330

    refactoring recipe object by DRY principle both in loadRecipe and uploadRecipe

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -660,6 +660,7 @@ const controlAddRecipe = async function(newRecipe) {
     try {
         // Upload new recipe data
         await _modelJs.uploadRecipe(newRecipe);
+        console.log(_modelJs.state.recipe);
     } catch (err) {
         console.error("\uD83D\uDCA5", err);
         (0, _addRecipeViewJsDefault.default).renderError(err.message);
@@ -1950,20 +1951,23 @@ const state = {
     },
     bookmarks: []
 };
+const createRecipeObject = function(data) {
+    const { recipe } = data.data;
+    return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        sourceUrl: recipe.source_url,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients
+    };
+};
 const loadRecipe = async function(id) {
     try {
         const data = await (0, _helpersJs.getJSON)(`${(0, _configJs.API_URL)}${id}`);
-        const { recipe } = data.data;
-        state.recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            cookingTime: recipe.cooking_time,
-            ingredients: recipe.ingredients
-        };
+        state.recipe = createRecipeObject(data);
         if (state.bookmarks.some((bookmark)=>bookmark.id === id)) state.recipe.bookmarked = true;
         else state.recipe.bookmarked = false;
     } catch (error) {
@@ -2049,7 +2053,7 @@ const uploadRecipe = async function(newRecipe) {
             ingredients: newRecipe.ingredients
         };
         const data = await (0, _helpersJs.sendJSON)(`${(0, _configJs.API_URL)}?key=${(0, _configJs.API_KEY)}`, recipe);
-        console.log(data);
+        state.recipe = createRecipeObject(data);
     } catch (err) {
         throw err;
     }
--- src/js/controller.js
+++ src/js/controller.js
@@ -87,6 +87,7 @@ const controlAddRecipe = async function (newRecipe) {
     try {
         // Upload new recipe data
         await model.uploadRecipe(newRecipe);
+        console.log(model.state.recipe);
     } catch (err) {
         console.error('💥', err);
         addRecipeView.renderError(err.message);
