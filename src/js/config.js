d01a06642e4404d027f5debe721ba966529602d5
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-10-20 21:58:56 +0330

    rebuild - 6 : fixing warnings and errors and adding .env file

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,7 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const API_KEY = '5e47f10d-aa86-44a1-81b5-a0f07055eb70';
-export const MODAL_CLOSE_SEC = 0.1;
+require('dotenv').config();
+
+export const API_URL = process.env.API_URL || 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = Number(process.env.TIMEOUT_SEC) || 10;
+export const RES_PER_PAGE = Number(process.env.RES_PER_PAGE) || 10;
+export const API_KEY = process.env.API_KEY || '5e47f10d-aa86-44a1-81b5-a0f07055eb70';
+export const MODAL_CLOSE_SEC = Number(process.env.MODAL_CLOSE_SEC) || 0.1;
